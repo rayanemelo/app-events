02ae6a9924494788305f34122275f794b93b4a84
rayane.melo <rayane.melo@lighthouseit.com.br>
2022-06-18 18:12:49 -0300

    feat: screen event

--- src/screens/Event/index.tsx
+++ src/screens/Event/index.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {useNavigation} from '@react-navigation/native';
 import {RootStackParamsList} from '../RootStackParams';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import {Container, Title, Text, Bold} from './styles';
 import Header from '../../components/Header';
+import Button from '../../components/Button';
 
 type EventScreenProp = NativeStackNavigationProp<RootStackParamsList, 'Event'>;
 
@@ -13,7 +15,33 @@ const Event: React.FC = () => {
   return (
     <>
       <Header text="Nome do evento" hasIcon={true} />
-      <View></View>
+      <Container>
+        <View>
+          <Title>Nome do evento</Title>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </Text>
+          <Bold>Data</Bold>
+          <Text>20/05/2022</Text>
+          <Bold>Horário</Bold>
+          <Text>20h30</Text>
+          <Bold>Localização</Bold>
+          <Text>
+            Theatro São Pedro - Praça Mal. Deodoro, S/N - Centro Histórico,
+            Porto Alegre - RS, 90010-300
+          </Text>
+          <Bold>Valor do ingresso</Bold>
+          <Text>R$130,00</Text>
+          <Bold>Informações de contato</Bold>
+          <Text>(51) 355496555, email@email.com </Text>
+        </View>
+        <View>
+          <Button text="Comprar ingresso" variantColor="blue" />
+        </View>
+      </Container>
     </>
   );
 };
---
+++ src/screens/Event/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 20px;
+  justify-content: space-between;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #222;
+  text-align: center;
+  margin-bottom: 10px;
+`;
+
+export const Bold = styled.Text`
+  font-size: 16px;
+  color: #222;
+  font-weight: bold;
+`;
+
+export const Text = styled.Text`
+  font-size: 16px;
+  color: #222;
+  text-align: justify;
+  margin-bottom: 10px;
+`;
