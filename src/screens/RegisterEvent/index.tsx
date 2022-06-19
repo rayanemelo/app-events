b993ab31a11cada408fdf0a2ee3aab67c015735c
rayane.melo <rayane.melo@lighthouseit.com.br>
2022-06-19 15:58:52 -0300

    feat: modal

--- src/screens/RegisterEvent/index.tsx
+++ src/screens/RegisterEvent/index.tsx
@@ -2,25 +2,26 @@ import React, {useState} from 'react';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {useNavigation} from '@react-navigation/native';
 import {RootStackParamsList} from '../RootStackParams';
-import {TouchableOpacityBase, View} from 'react-native';
+import {TouchableOpacity, View, Text, Modal} from 'react-native';
 import Header from '../../components/Header';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import {Container, Descripcion, Date} from './styles';
-import DatePicker from 'react-native-date-picker';
+import {
+  Container,
+  Descripcion,
+  ContainerModal,
+  TextModal,
+  CloseModal,
+} from './styles';
 
 type RegisterEventScreenProp = NativeStackNavigationProp<
   RootStackParamsList,
   'RegisterEvent'
 >;
 
-interface IDatePicker {
-  date: Date;
-}
-
 const RegisterEvent: React.FC = () => {
   const navigation = useNavigation<RegisterEventScreenProp>();
-  // const [date, setDate] = useState(new Date());
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <>
       <Header text="Cadastrar Evento" hasIcon={true} />
@@ -35,8 +36,21 @@ const RegisterEvent: React.FC = () => {
           <Input placeholder="Informações de contato" />
         </View>
         <View>
-          <Button text="Salvar" variantColor="blue" />
+          <Button
+            text="Salvar"
+            variantColor="blue"
+            onPress={() => setModalVisible(true)}
+          />
         </View>
+        <Modal animationType="fade" transparent={true} visible={modalVisible}>
+          <ContainerModal>
+            <TextModal>Cadastro salvo com sucesso!</TextModal>
+            <TextModal>Erro ao criar evento</TextModal>
+            <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
+              <CloseModal>Fechar</CloseModal>
+            </TouchableOpacity>
+          </ContainerModal>
+        </Modal>
       </Container>
     </>
   );
--- src/screens/RegisterEvent/styles.ts
+++ src/screens/RegisterEvent/styles.ts
@@ -16,12 +16,29 @@ export const Descripcion = styled.TextInput`
   padding-left:10px
 `;
 
-export const Date = styled.TouchableOpacity`
-  width: 100%;
-  height: 48px;
-  background-color: #fff;
-  border: 2px solid #1c658c
-  margin: 5px 0; 
-  border-radius:8px;
-  padding-left:10px
+export const ContainerModal = styled.View`
+ padding: 20px
+  width: 80%;
+  background-color: #1c658c;
+  border-radius: 8px;
+  justify-content: center;
+  align-items: center;
+  margin: 150px auto;
+  elevation: 5;
+`;
+
+export const TextModal = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 16px;
+`;
+
+export const CloseModal = styled.Text`
+  color: #1c658c;
+  height: 30px;
+  background-color: #ddd;
+  border-radius: 8px;
+  margin-top: 15px;
+  text-align: center;
+  padding: 5px;
 `;
